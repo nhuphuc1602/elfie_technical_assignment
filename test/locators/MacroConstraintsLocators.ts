46914ccc5f186034cd2c0bb5fdd29e05a472cbdb
Phuc Vo <phuc.vo@skymavis.com>
2025-03-27 23:35:18 +0700

    fix ignorecase git

---
+++ test/locators/MacroConstraintsLocators.ts
@@ -0,0 +1,9 @@
+export const MacroConstraintsLocators = {
+    addConstraintTitle: '//android.widget.TextView[@text="Add constraint"]]',
+    deviceStateButton: '//android.widget.TextView[@resource-id="com.arlosoft.macrodroid:id/category_name" and @text="Device State"]',
+    airplanemodeButton: '//android.widget.TextView[@resource-id="com.arlosoft.macrodroid:id/select_item_name" and @text="Airplane Mode"]',
+    airplanemodeDisabledRadioButton: '//android.widget.CheckedTextView[@resource-id="android:id/text1" and @text="Airplane Mode Disabled"]',
+    okButton: '//android.widget.Button[@text="OK"]',
+    cancelButton: '//android.widget.Button[@text="CANCEL"]',
+    anyAppRadioButton: '//android.widget.CheckedTextView[@resource-id="android:id/text1" and @text="Any Application"]',
+};
