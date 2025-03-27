821984c6494cae3aadf52acd8d4daef69b4aa26d
Phuc Vo <phuc.vo@skymavis.com>
2025-03-27 23:45:41 +0700

    fix name case

--- test/locators/HomeLocators1.ts
+++
@@ -1,8 +0,0 @@
-export const HomeLocators = {
-    homeTitle: '//android.widget.TextView[@text="MACRODROID"]',
-    macroButton: '//android.widget.TextView[@resource-id="com.arlosoft.macrodroid:id/title" and @text="Add Macro"]',
-    actionBlocksButton: '//android.widget.TextView[@resource-id="com.arlosoft.macrodroid:id/title" and @text="Action Blocks"]',
-    macroWizardButton: '//android.widget.TextView[@resource-id="com.arlosoft.macrodroid:id/title" and @text="Add Macro Wizard"]',
-    macrosTab: '//android.widget.FrameLayout[@content-desc="Macros"]',
-    macrosNameText: '//android.widget.TextView[@resource-id="com.arlosoft.macrodroid:id/macroNameText"]'
-};
