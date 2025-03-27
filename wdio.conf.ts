49640b4b314aecea86f52e69e804855badf3f066
Phuc Vo <phuc.vo@skymavis.com>
2025-03-27 23:57:30 +0700

    improve wdio conf

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,25 +1,5 @@
-// import './test/support/hooks'; 
-
-// export const config: WebdriverIO.Config = {
-//     runner: 'local',
-//     framework: 'cucumber',
-//     specs: ['./features/**/*.feature'],
-//     cucumberOpts: {
-//         require: ['./test/support/hooks.ts', './test/steps/*.ts'],
-//         timeout: 60000
-//     },
-//     capabilities: [{
-//         platformName: 'Android',
-//         'appium:deviceName': 'Pixel 9',
-//         'appium:platformVersion': '15.0',
-//         'appium:automationName': 'UiAutomator2',
-//         'appium:app': './apps/Final_Exam_MacroDroid.apk',
-//         'appium:noReset': true
-//     }],
-//     services: ['appium'],
-//     port: 4723
-// };
 import { appiumConfig } from './config';
+
 const FRAMEWORK = appiumConfig.framework;
 const isCucumber = FRAMEWORK === ('cucumber' as typeof FRAMEWORK);
 
